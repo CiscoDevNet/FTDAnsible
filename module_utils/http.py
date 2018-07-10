cb21de829ed49d95cba7d6a6a7c515e8207102a1
Anton Nikulin <anton.nikulin@teamdev.com>
2018-07-10 19:30:14 +0300

    Updated API version

--- module_utils/http.py
+++ module_utils/http.py
@@ -2,7 +2,7 @@ import time
 from ansible.module_utils.facts.timeout import TimeoutError
 from ansible.module_utils.six.moves.urllib.parse import urlencode
 
-API_PREFIX = "/api/fdm/v1"
+API_PREFIX = "/api/fdm/v2"
 
 DEFAULT_PAGE_SIZE = 10
 DEFAULT_TIMEOUT = 60 * 10  # 10 minutes
