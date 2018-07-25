32d7c25de15aca2d8c5a8b73c7ce73d2fbd340b5
Anton Nikulin <anton.nikulin@teamdev.com>
2018-07-25 15:34:30 +0300

    Initial functionality for dict comparison

--- module_utils/misc.py
+++ module_utils/misc.py
@@ -32,3 +32,26 @@ def copy_identity_properties(source_obj, dest_obj):
     if 'ruleId' in source_obj:
         dest_obj['ruleId'] = source_obj['ruleId']
     return dest_obj
+
+
+def equal_dicts(dict1, dict2):
+    if type(dict1) is not dict or type(dict2) is not dict:
+        raise ValueError("Arguments must be dictionaries")
+
+    if len(dict1) != len(dict2):
+        return False
+
+    for key, value1 in dict1.items():
+        if key not in dict2:
+            return False
+
+        value2 = dict2[key]
+
+        if type(value1) != type(value2):
+            return False
+
+        equal_values = value1 == value2 if type(value1) != dict else equal_dicts(value1, value2)
+        if not equal_values:
+            return False
+
+    return True
---
+++ test/unit/test_misc.py
@@ -0,0 +1,57 @@
+import pytest
+
+from module_utils.misc import equal_dicts
+
+
+def test_equal_dicts_raise_error_with_wrong_args():
+    with pytest.raises(ValueError, match='Arguments must be dictionaries'):
+        equal_dicts({}, 'a')
+
+
+def test_equal_dicts_return_false_with_different_length():
+    assert not equal_dicts(
+        {'foo': 1},
+        {'foo': 1, 'bar': 2}
+    )
+
+
+def test_equal_dicts_return_false_with_different_fields():
+    assert not equal_dicts(
+        {'foo': 1},
+        {'bar': 1}
+    )
+
+
+def test_equal_dicts_return_false_with_different_value_types():
+    assert not equal_dicts(
+        {'foo': 1},
+        {'foo': '1'}
+    )
+
+
+def test_equal_dicts_return_false_with_different_values():
+    assert not equal_dicts(
+        {'foo': 1},
+        {'foo': 2}
+    )
+
+
+def test_equal_dicts_return_false_with_different_nested_values():
+    assert not equal_dicts(
+        {'foo': {'bar': 1}},
+        {'foo': {'bar': 2}}
+    )
+
+
+def test_equal_dicts_return_true_with_equal_dicts():
+    assert equal_dicts(
+        {'foo': 1, 'bar': 2},
+        {'bar': 2, 'foo': 1}
+    )
+
+
+def test_equal_dicts_return_true_with_equal_nested_dicts():
+    assert equal_dicts(
+        {'foo': {'bar': 1, 'buz': 2}},
+        {'foo': {'buz': 2, 'bar': 1}}
+    )
