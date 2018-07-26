9b511fe5975f72947d567a167e3dbc87abb80ae9
Deepak Agrawal <deepacks@gmail.com>
2018-07-26 07:15:18 +0000

    refactor httpapi plugin after HTTP error and token expiry are handled in connection plugin

--- httpapi_plugins/ftd.py
+++ httpapi_plugins/ftd.py
@@ -33,22 +33,6 @@ API_TOKEN_PATH = "/fdm/token"
 TOKEN_EXPIRATION_STATUS_CODE = 408
 UNAUTHORIZED_STATUS_CODE = 401
 
-
-def retry_on_token_expiration(func):
-    @wraps(func)
-    def f_retry(self, *args, **kwargs):
-        try:
-            return func(self, *args, **kwargs)
-        except HTTPError as e:
-            if e.code == TOKEN_EXPIRATION_STATUS_CODE or e.code == UNAUTHORIZED_STATUS_CODE:
-                self._refresh_token()
-                return func(self, *args, **kwargs)
-            else:
-                raise e
-
-    return f_retry
-
-
 class HttpApi(HttpApiBase):
     def __init__(self, connection):
         self.connection = connection
@@ -56,41 +40,47 @@ class HttpApi(HttpApiBase):
         self.refresh_token = False
 
     def login(self, username, password):
-        auth_payload = {
-            'grant_type': 'password',
-            'username': username,
-            'password': password
-        }
-        response = self.connection.send(API_PREFIX + API_TOKEN_PATH, json.dumps(auth_payload), method='POST',
-                                        headers=BASE_HEADERS)
-        self._set_token_info(response)
+        if self.refresh_token:
+            payload = {
+                'grant_type': 'refresh_token',
+                'refresh_token': self.refresh_token
+            }
+        else:
+            payload = {
+                'grant_type': 'password',
+                'username': username,
+                'password': password
+            }
+        response = self.connection.send(
+            API_PREFIX + API_TOKEN_PATH,
+            json.dumps(payload), method='POST', headers=BASE_HEADERS
+        )
 
-    @retry_on_token_expiration
     def send_request(self, url_path, http_method, body_params=None, path_params=None, query_params=None):
         url = construct_url_path(url_path, path_params, query_params)
         data = json.dumps(body_params) if body_params else None
-        response = self.connection.send(url, data, method=http_method, headers=self._authorized_headers()).read()
-        return json.loads(to_text(response)) if response else response
+        response = self.connection.send(
+            url, data, method=http_method,
+            headers=BASE_HEADERS
+        ).read()
+        ret = json.loads(to_text(response)) if response else response
+        return ret
 
-    @retry_on_token_expiration
     def upload_file(self, from_path, to_url):
         url = construct_url_path(to_url)
         with open(from_path, 'rb') as src_file:
             rf = RequestField('fileToUpload', src_file.read(), os.path.basename(src_file.name))
             rf.make_multipart()
             body, content_type = encode_multipart_formdata([rf])
-
-            headers = self._authorized_headers()
             headers['Content-Type'] = content_type
             headers['Content-Length'] = len(body)
-
             response = self.connection.send(url, data=body, method='POST', headers=headers).read()
         return json.loads(to_text(response))
 
-    @retry_on_token_expiration
     def download_file(self, from_url, to_path):
         url = construct_url_path(from_url)
-        response = self.connection.send(url, data=None, method='GET', headers=self._authorized_headers())
+        response = self.connection.send(url, data=None, method='GET',
+                                        headers=BASE_HEADERS)
 
         if os.path.isdir(to_path):
             filename = extract_filename_from_headers(response.info())
@@ -99,23 +89,15 @@ class HttpApi(HttpApiBase):
         with open(to_path, "wb") as output_file:
             shutil.copyfileobj(response, output_file)
 
-    def _authorized_headers(self):
-        headers = dict(BASE_HEADERS)
-        headers['Authorization'] = 'Bearer %s' % self.access_token
-        return headers
-
-    def _refresh_token(self):
-        payload = {
-            'grant_type': 'refresh_token',
-            'refresh_token': self.refresh_token
-        }
-        response = self.connection.send(API_PREFIX + API_TOKEN_PATH, json.dumps(payload), method='POST', headers=BASE_HEADERS)
-        self._set_token_info(response)
-
-    def _set_token_info(self, token_response):
-        token_info = json.loads(to_text(token_response.read()))
-        self.refresh_token = token_info['refresh_token']
-        self.access_token = token_info['access_token']
+    def update_auth(self, response):
+        for keys in response.info():
+        token_info = json.loads(to_text(response.read()))
+        if 'refresh_token' in token_info:
+            self.refresh_token = token_info['refresh_token']
+        if 'access_token' in token_info:
+            self.access_token = token_info['access_token']
+            return {'Authorization' : 'Bearer %s' % token_info['access_token']}
+        return None
 
     def logout(self):
         # Revoke the tokens
@@ -132,6 +114,8 @@ class HttpApi(HttpApiBase):
                 .format(logout_info['message'])
             )
         else:
+            self.refresh_token = False
+            self.access_token = False
             display.vvvv("logged out successfully")
 
 def construct_url_path(path, path_params=None, query_params=None):
