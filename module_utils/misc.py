94c0e991b4dddcea2d8ac33ec8a28c39a0f3510d
Anton Nikulin <anton.nikulin@teamdev.com>
2018-07-26 19:00:24 +0300

    Updated helper to construct ansible facts

--- module_utils/misc.py
+++ module_utils/misc.py
@@ -8,21 +8,17 @@ def dict_subset(dictionary, keys):
     return dict((k, dictionary[k]) for k in keys if dictionary[k])
 
 
-def construct_module_result(response, params):
-    def register_return_fact(facts):
-        if response:
-            response_body = response['items'] if 'items' in response else response
-            if params.get('register_as'):
-                facts[params['register_as']] = response_body
-            elif 'name' in response_body and 'type' in response_body:
-                object_name = re.sub(INVALID_IDENTIFIER_SYMBOLS, '_', response_body['name'].lower())
-                fact_name = '%s_%s' % (response_body['type'], object_name)
-                facts[fact_name] = response_body
-        return facts
-
-    result = dict(changed=True, response=response, ansible_facts=dict())
-    result['ansible_facts'] = register_return_fact(result['ansible_facts'])
-    return result
+def construct_ansible_facts(response, params):
+    facts = dict()
+    if response:
+        response_body = response['items'] if 'items' in response else response
+        if params.get('register_as'):
+            facts[params['register_as']] = response_body
+        elif 'name' in response_body and 'type' in response_body:
+            object_name = re.sub(INVALID_IDENTIFIER_SYMBOLS, '_', response_body['name'].lower())
+            fact_name = '%s_%s' % (response_body['type'], object_name)
+            facts[fact_name] = response_body
+    return facts
 
 
 def copy_identity_properties(source_obj, dest_obj):
