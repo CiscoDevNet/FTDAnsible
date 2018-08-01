7977b4a9370c3d6a8d39851d627f08488d2a5378
Anton Nikulin <anton.nikulin@teamdev.com>
2018-08-01 19:24:19 +0300

    Updated comparison function to understand object references

--- module_utils/config_resource.py
+++ module_utils/config_resource.py
@@ -2,9 +2,8 @@ from functools import partial
 
 from ansible.module_utils._text import to_text
 from ansible.module_utils.http import iterate_over_pageable_resource
+from ansible.module_utils.misc import equal_objects
 from ansible.module_utils.six.moves.urllib.error import HTTPError
-
-IGNORED_FIELDS = ['id', 'version', 'isSystemDefined', 'links']
 
 
 class BaseConfigObjectResource(object):
@@ -13,7 +12,8 @@ class BaseConfigObjectResource(object):
         self.config_changed = False
 
     def get_object_by_name(self, url_path, name, path_params=None):
-        get_object_list = partial(self._conn.send_request, url_path=url_path, http_method='GET', path_params=path_params)
+        get_object_list = partial(self._conn.send_request, url_path=url_path, http_method='GET',
+                                  path_params=path_params)
 
         item_generator = iterate_over_pageable_resource(
             lambda query_params: get_object_list(query_params=query_params),
@@ -31,7 +31,8 @@ class BaseConfigObjectResource(object):
         elif equal_objects(existing_obj, body_params):
             return existing_obj
         else:
-            raise ValueError('Cannot add new object. An object with the same name but different parameters already exists.')
+            raise ValueError(
+                'Cannot add new object. An object with the same name but different parameters already exists.')
 
     def delete_object(self, url_path, path_params):
         def is_invalid_uuid_error(err):
@@ -62,29 +63,3 @@ class BaseConfigObjectResource(object):
                                            path_params=path_params, query_params=query_params)
         self.config_changed = True
         return response
-
-
-def equal_objects(dict1, dict2):
-    if type(dict1) is not dict or type(dict2) is not dict:
-        raise ValueError("Arguments must be dictionaries")
-
-    dict1 = dict((k, dict1[k]) for k in dict1.keys() if k not in IGNORED_FIELDS and dict1[k])
-    dict2 = dict((k, dict2[k]) for k in dict2.keys() if k not in IGNORED_FIELDS and dict2[k])
-
-    if len(dict1) != len(dict2):
-        return False
-
-    for key, value1 in dict1.items():
-        if key not in dict2:
-            return False
-
-        value2 = dict2[key]
-
-        if type(value1) != type(value2):
-            return False
-
-        equal_values = value1 == value2 if type(value1) != dict else equal_objects(value1, value2)
-        if not equal_values:
-            return False
-
-    return True
--- module_utils/misc.py
+++ module_utils/misc.py
@@ -1,6 +1,7 @@
 import re
 
 INVALID_IDENTIFIER_SYMBOLS = r'[^a-zA-Z0-9_]'
+IGNORED_FIELDS = ['id', 'version', 'isSystemDefined', 'links', 'eventLogAction', 'ruleAction', 'ruleId']
 
 
 def dict_subset(dictionary, keys):
@@ -28,3 +29,74 @@ def copy_identity_properties(source_obj, dest_obj):
     if 'ruleId' in source_obj:
         dest_obj['ruleId'] = source_obj['ruleId']
     return dest_obj
+
+
+def is_object_ref(d):
+    if type(d) is not dict:
+        return False
+
+    has_id = 'id' in d.keys() and d['id']
+    has_type = 'type' in d.keys() and d['type']
+    return has_id and has_type
+
+
+def equal_object_refs(d1, d2):
+    have_equal_ids = d1['id'] == d2['id']
+    have_equal_types = d1['type'] == d2['type']
+    return have_equal_ids and have_equal_types
+
+
+def equal_objects(dict1, dict2):
+    def _equal_lists(l1, l2):
+        import json
+        with open('/tmp/test1', 'wt') as f1:
+            f1.write(json.dumps(l1))
+        with open('/tmp/test2', 'wt') as f2:
+            f2.write(json.dumps(l2))
+
+        if len(l1) != len(l2):
+            return False
+
+        for v1, v2 in zip(l1, l2):
+            if type(v1) != type(v2):
+                return False
+
+            if is_object_ref(v1) and is_object_ref(v2):
+                equal_vs = equal_object_refs(v1, v2)
+            else:
+                equal_vs = v1 == v2
+
+            if not equal_vs:
+                return False
+
+        return True
+
+    if type(dict1) is not dict or type(dict2) is not dict:
+        raise ValueError("Arguments must be dictionaries")
+
+    dict1 = dict((k, dict1[k]) for k in dict1.keys() if k not in IGNORED_FIELDS and dict1[k])
+    dict2 = dict((k, dict2[k]) for k in dict2.keys() if k not in IGNORED_FIELDS and dict2[k])
+
+    if len(dict1) != len(dict2):
+        return False
+
+    for key, value1 in dict1.items():
+        if key not in dict2:
+            return False
+
+        value2 = dict2[key]
+
+        if type(value1) != type(value2):
+            return False
+
+        if type(value1) == list:
+            equal_values = _equal_lists(value1, value2)
+        elif is_object_ref(value1) and is_object_ref(value2):
+            equal_values = equal_object_refs(value1, value2)
+        else:
+            equal_values = value1 == value2
+
+        if not equal_values:
+            return False
+
+    return True
