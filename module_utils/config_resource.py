c3b3966f7cf0468bad881c1c5453491c5a42f6fa
Anton Nikulin <anton.nikulin@teamdev.com>
2018-08-03 16:37:39 +0300

    Use enum for HTTP methods

--- module_utils/config_resource.py
+++ module_utils/config_resource.py
@@ -1,9 +1,17 @@
+from enum import Enum
 from functools import partial
 
-from ansible.module_utils._text import to_text
+from ansible.module_utils.basic import to_text
 from ansible.module_utils.http import iterate_over_pageable_resource
 from ansible.module_utils.misc import equal_objects
 from ansible.module_utils.six.moves.urllib.error import HTTPError
+
+
+class HTTPMethod(Enum):
+    GET = 'GET'
+    POST = 'POST'
+    PUT = 'PUT'
+    DELETE = 'DELETE'
 
 
 class BaseConfigObjectResource(object):
@@ -12,7 +20,7 @@ class BaseConfigObjectResource(object):
         self.config_changed = False
 
     def get_object_by_name(self, url_path, name, path_params=None):
-        get_object_list = partial(self._conn.send_request, url_path=url_path, http_method='GET',
+        get_object_list = partial(self._send_request, url_path=url_path, http_method=HTTPMethod.GET,
                                   path_params=path_params)
 
         item_generator = iterate_over_pageable_resource(
@@ -26,8 +34,8 @@ class BaseConfigObjectResource(object):
         existing_obj = self.get_object_by_name(url_path, body_params['name'], path_params)
 
         if not existing_obj:
-            return self._send_modifying_request(url_path=url_path, http_method='POST', body_params=body_params,
-                                                path_params=path_params, query_params=query_params)
+            return self._send_request(url_path=url_path, http_method=HTTPMethod.POST, body_params=body_params,
+                                      path_params=path_params, query_params=query_params)
         elif equal_objects(existing_obj, body_params):
             return existing_obj
         else:
@@ -40,7 +48,7 @@ class BaseConfigObjectResource(object):
             return err.code == 422 and "Validation failed due to an invalid UUID" in err_msg
 
         try:
-            return self._send_modifying_request(url_path=url_path, http_method='DELETE', path_params=path_params)
+            return self._send_request(url_path=url_path, http_method=HTTPMethod.DELETE, path_params=path_params)
         except HTTPError as e:
             if is_invalid_uuid_error(e):
                 return {'response': 'Referenced object does not exist'}
@@ -48,18 +56,19 @@ class BaseConfigObjectResource(object):
                 raise e
 
     def edit_object(self, url_path, body_params, path_params=None, query_params=None):
-        existing_object = self._conn.send_request(url_path=url_path, http_method='GET', path_params=path_params)
+        existing_object = self._send_request(url_path=url_path, http_method=HTTPMethod.GET, path_params=path_params)
 
         if not existing_object:
             raise ValueError('Referenced object does not exist')
         elif equal_objects(existing_object, body_params):
             return existing_object
         else:
-            return self._send_modifying_request(url_path=url_path, http_method='PUT', body_params=body_params,
-                                                path_params=path_params, query_params=query_params)
+            return self._send_request(url_path=url_path, http_method=HTTPMethod.PUT, body_params=body_params,
+                                      path_params=path_params, query_params=query_params)
 
-    def _send_modifying_request(self, url_path, http_method, body_params=None, path_params=None, query_params=None):
-        response = self._conn.send_request(url_path=url_path, http_method=http_method, body_params=body_params,
+    def _send_request(self, url_path, http_method, body_params=None, path_params=None, query_params=None):
+        response = self._conn.send_request(url_path=url_path, http_method=http_method.value, body_params=body_params,
                                            path_params=path_params, query_params=query_params)
-        self.config_changed = True
+        if http_method != HTTPMethod.GET:
+            self.config_changed = True
         return response
