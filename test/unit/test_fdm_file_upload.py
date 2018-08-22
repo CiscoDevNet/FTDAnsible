d14bc7ac228402a242c62268929a4deefd5d5856
Anton Nikulin <anton.nikulin@teamdev.com>
2018-08-22 16:09:55 +0300

    Added unit tests for upload module

--- test/unit/test_fdm_file_upload.py
+++ test/unit/test_fdm_file_upload.py
@@ -4,13 +4,13 @@ import pytest
 from ansible.module_utils import basic
 from units.modules.utils import set_module_args, exit_json, fail_json, AnsibleFailJson, AnsibleExitJson
 
-from library import ftd_file_download
-from module_utils.fdm_swagger_client import FILE_MODEL_NAME, OperationField
+from library import ftd_file_upload
+from module_utils.fdm_swagger_client import OperationField
 from module_utils.http import HTTPMethod
 
 
-class TestFtdFileDownload(object):
-    module = ftd_file_download
+class TestFtdFileUpload(object):
+    module = ftd_file_upload
 
     @pytest.fixture(autouse=True)
     def module_mock(self, mocker):
@@ -18,12 +18,12 @@ class TestFtdFileDownload(object):
 
     @pytest.fixture
     def connection_mock(self, mocker):
-        connection_class_mock = mocker.patch('library.ftd_file_download.Connection')
+        connection_class_mock = mocker.patch('library.ftd_file_upload.Connection')
         return connection_class_mock.return_value
 
-    @pytest.mark.parametrize("missing_arg", ['operation', 'destination'])
+    @pytest.mark.parametrize("missing_arg", ['operation', 'fileToUpload'])
     def test_module_should_fail_without_required_args(self, missing_arg):
-        module_args = {'operation': 'downloadFile', 'destination': '/tmp'}
+        module_args = {'operation': 'uploadFile', 'fileToUpload': '/tmp/test.txt'}
         del module_args[missing_arg]
         set_module_args(module_args)
 
@@ -34,46 +34,47 @@ class TestFtdFileDownload(object):
 
     def test_module_should_fail_when_no_operation_spec_found(self, connection_mock):
         connection_mock.get_operation_spec.return_value = None
-        set_module_args({'operation': 'nonExistingDownloadOperation', 'destination': '/tmp'})
+        set_module_args({'operation': 'nonExistingUploadOperation', 'fileToUpload': '/tmp/test.txt'})
 
         with pytest.raises(AnsibleFailJson) as ex:
             self.module.main()
 
         result = ex.value.args[0]
         assert result['failed']
-        assert result['msg'] == 'Operation with specified name is not found: nonExistingDownloadOperation'
+        assert result['msg'] == 'Operation with specified name is not found: nonExistingUploadOperation'
 
-    def test_module_should_fail_when_not_download_operation_specified(self, connection_mock):
+    def test_module_should_fail_when_not_upload_operation_specified(self, connection_mock):
         connection_mock.get_operation_spec.return_value = {
             OperationField.METHOD: HTTPMethod.GET,
-            OperationField.URL: '/object',
+            OperationField.URL: '/object/network',
             OperationField.MODEL_NAME: 'NetworkObject'
         }
-        set_module_args({'operation': 'nonDownloadOperation', 'destination': '/tmp'})
+        set_module_args({'operation': 'nonUploadOperation', 'fileToUpload': '/tmp/test.txt'})
 
         with pytest.raises(AnsibleFailJson) as ex:
             self.module.main()
 
         result = ex.value.args[0]
         assert result['failed']
-        assert result['msg'] == 'Invalid download operation: nonDownloadOperation. ' \
-                                'The operation must make GET request and return a file.'
+        assert result['msg'] == 'Invalid upload operation: nonUploadOperation. ' \
+                                'The operation must make POST request and return UploadStatus model.'
 
-    def test_module_should_call_download_and_return(self, connection_mock):
+    def test_module_should_call_upload_and_return_response(self, connection_mock):
         connection_mock.get_operation_spec.return_value = {
-            OperationField.METHOD: HTTPMethod.GET,
-            OperationField.URL: '/file/{objId}',
-            OperationField.MODEL_NAME: FILE_MODEL_NAME
+            OperationField.METHOD: HTTPMethod.POST,
+            OperationField.URL: '/uploadFile',
+            OperationField.MODEL_NAME: 'FileUploadStatus'
         }
+        connection_mock.upload_file.return_value = {'id': '123'}
 
         set_module_args({
-            'operation': 'downloadFile',
-            'path_params': {'objId': '12'},
-            'destination': '/tmp'
+            'operation': 'uploadFile',
+            'fileToUpload': '/tmp/test.txt'
         })
         with pytest.raises(AnsibleExitJson) as ex:
             self.module.main()
 
         result = ex.value.args[0]
-        assert not result['changed']
-        connection_mock.download_file.assert_called_once_with('/file/{objId}', '/tmp', {'objId': '12'})
+        assert result['changed']
+        assert {'id': '123'} == result['response']
+        connection_mock.upload_file.assert_called_once_with('/tmp/test.txt', '/uploadFile')
