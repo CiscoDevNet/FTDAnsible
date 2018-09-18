d54eac870fe07c522afcf59b60686f2fd2edb011
Anton Nikulin <anton.nikulin@teamdev.com>
2018-09-18 13:47:21 +0300

    Add model parameter to generator script

--- docs/generator.py
+++ docs/generator.py
@@ -23,7 +23,7 @@ MODELS_FOLDER = os.path.join(DIR_PATH, 'models')
 OPERATIONS_FOLDER = os.path.join(DIR_PATH, 'operations')
 INDEX_FILE = 'index.rst'
 
-TOKEN_PATH = '/api/fdm/v1/fdm/token'
+TOKEN_PATH = '/api/fdm/v2/fdm/token'
 SPEC_PATH = '/apispec/ngfw.json'
 DOC_PATH = '/apispec/en-us/doc.json'
 
@@ -42,48 +42,46 @@ def clean_generated_files(dir_path):
             os.remove(os.path.join(dir_path, file_name))
 
 
-def generate_model_index(api_spec, docs, model_template):
-    def group_operations_by_model():
-        model_operations = {}
-        for operation_name, params in api_spec[SpecProp.OPERATIONS].items():
-            model_name = params[OperationField.MODEL_NAME]
-            model_operations.setdefault(model_name, {})[operation_name] = params
-        return model_operations
-
+def generate_model_index(api_spec, docs, model_template, include_models=None):
     clean_generated_files(MODELS_FOLDER)
 
-    operations_by_model = group_operations_by_model()
-    for model_name in operations_by_model.keys():
-        # delete operations used not to have a model, but the issue is fixed in
-        # https://github.com/CiscoDevNet/FTDAnsible/pull/21
-        if not model_name:
-            continue
-
+    for model_name, operations in api_spec[SpecProp.MODEL_OPERATIONS].items():
         model_def = docs[SpecProp.DEFINITIONS].get(model_name, {})
         model_spec = ModelSpec(
             model_name,
             model_def.get('description', ''),
-            operations_by_model.get(model_name)
+            operations.keys()
         )
+
+        ignore_model = include_models and model_name not in include_models
+        # TODO: investigate why some operations still have None model name
+        if model_name is None or ignore_model:
+            continue
+
         with open('%s/%s.rst' % (MODELS_FOLDER, camel_to_snake(model_name)), "wb") as f:
             f.write(model_template.render(
                 model=model_spec
             ).encode('utf-8'))
 
 
-def generate_operation_index(api_spec, docs, operation_template):
+def generate_operation_index(api_spec, docs, operation_template, include_models=None):
     def enrich_params_with_description(op_spec, op_docs):
         param_descriptions = {p['name']: p['description'] for p in op_docs.get('parameters', {})}
-        for param_name, params_spec in op_spec[OperationField.PARAMETERS][OperationParams.PATH].items():
-            params_spec['description'] = param_descriptions.get(param_name, '')
-        for param_name, params_spec in op_spec[OperationField.PARAMETERS][OperationParams.QUERY].items():
-            params_spec['description'] = param_descriptions.get(param_name, '')
+        if OperationField.PARAMETERS in op_spec:
+            for param_name, params_spec in op_spec[OperationField.PARAMETERS][OperationParams.PATH].items():
+                params_spec['description'] = param_descriptions.get(param_name, '')
+            for param_name, params_spec in op_spec[OperationField.PARAMETERS][OperationParams.QUERY].items():
+                params_spec['description'] = param_descriptions.get(param_name, '')
         return op_spec
 
     clean_generated_files(OPERATIONS_FOLDER)
 
     for op_name, op_spec in api_spec[SpecProp.OPERATIONS].items():
-        op_docs = docs[PropName.PATHS][op_spec[OperationField.URL][11:]][op_spec[OperationField.METHOD]]
+        ignore_op = include_models and op_spec[OperationField.MODEL_NAME] not in include_models
+        if ignore_op:
+            continue
+
+        op_docs = docs[PropName.PATHS].get(op_spec[OperationField.URL][11:], {}).get(op_spec[OperationField.METHOD], {})
         op_spec = enrich_params_with_description(op_spec, op_docs)
         operation = OperationSpec(
             op_name,
@@ -103,16 +101,17 @@ def fetch_api_specs(hostname, username, password):
             hostname + TOKEN_PATH,
             method=HTTPMethod.POST,
             data=json.dumps({'grant_type': 'password', 'username': username, 'password': password}),
-            headers=BASE_HEADERS
+            headers=BASE_HEADERS,
+            validate_certs=False
         ).read()
         return json.loads(to_text(resp))
 
     headers = dict(BASE_HEADERS)
     headers['Authorization'] = 'Bearer %s' % request_token()['access_token']
 
-    spec_resp = open_url(hostname + SPEC_PATH, method=HTTPMethod.GET, headers=headers).read()
+    spec_resp = open_url(hostname + SPEC_PATH, method=HTTPMethod.GET, headers=headers, validate_certs=False).read()
     api_spec = FdmSwaggerParser().parse_spec(json.loads(to_text(spec_resp)))
-    docs_resp = open_url(hostname + DOC_PATH, method=HTTPMethod.GET, headers=headers).read()
+    docs_resp = open_url(hostname + DOC_PATH, method=HTTPMethod.GET, headers=headers, validate_certs=False).read()
     docs = json.loads(to_text(docs_resp))
     return api_spec, docs
 
@@ -126,16 +125,17 @@ def init_jinja_env():
 
 def main():
     parser = argparse.ArgumentParser(description='Generates Ansible modules from Swagger documentation')
-    parser.add_argument('hostname', help='Hostname where FTD can be accessed')
-    parser.add_argument('username', help='FTD username that has access to Swagger docs')
-    parser.add_argument('password', help='Password for the username')
+    parser.add_argument('hostname', type=str, help='Hostname where FTD can be accessed')
+    parser.add_argument('username', type=str, help='FTD username that has access to Swagger docs')
+    parser.add_argument('password', type=str, help='Password for the username')
+    parser.add_argument('--models', type=str, nargs='+', help='A list of models to include in the docs', required=False)
     args = parser.parse_args()
 
     env = init_jinja_env()
     api_spec, docs = fetch_api_specs(args.hostname, args.username, args.password)
 
-    generate_model_index(api_spec, docs, env.get_template(MODEL_TEMPLATE))
-    generate_operation_index(api_spec, docs, env.get_template(OPERATION_TEMPLATE))
+    generate_model_index(api_spec, docs, env.get_template(MODEL_TEMPLATE), args.models)
+    generate_operation_index(api_spec, docs, env.get_template(OPERATION_TEMPLATE), args.models)
 
 
 if __name__ == '__main__':
