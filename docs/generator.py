356cea6c1b6df70d4a959bd6537d393e59101646
Anton Nikulin <anton.nikulin@teamdev.com>
2018-09-19 16:08:46 +0300

    Refactor doc generator

--- docs/generator.py
+++ docs/generator.py
@@ -15,14 +15,6 @@ from module_utils.fdm_swagger_client import FdmSwaggerParser, SpecProp, Operatio
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
-TEMPLATE_FOLDER = os.path.join(DIR_PATH, 'templates')
-MODEL_TEMPLATE = 'model.j2'
-OPERATION_TEMPLATE = 'operation.j2'
-
-MODELS_FOLDER = os.path.join(DIR_PATH, 'models')
-OPERATIONS_FOLDER = os.path.join(DIR_PATH, 'operations')
-INDEX_FILE = 'index.rst'
-
 TOKEN_PATH = '/api/fdm/v2/fdm/token'
 SPEC_PATH = '/apispec/ngfw.json'
 DOC_PATH = '/apispec/en-us/doc.json'
@@ -31,57 +23,79 @@ ModelSpec = namedtuple('ModelSpec', 'name description properties operations')
 OperationSpec = namedtuple('OperationSpec', 'name description path_params query_params')
 
 
+class DocGenerator(object):
+    TEMPLATE_FOLDER = os.path.join(DIR_PATH, 'templates')
+    MODELS_FOLDER = os.path.join(DIR_PATH, 'models')
+    OPERATIONS_FOLDER = os.path.join(DIR_PATH, 'operations')
+
+    MODEL_TEMPLATE = 'model.j2'
+    OPERATION_TEMPLATE = 'operation.j2'
+    INDEX_FILE = 'index.rst'
+
+    _api_spec = None
+    _jinja_env = None
+
+    def __init__(self, api_spec):
+        self._api_spec = api_spec
+        self._jinja_env = self._init_jinja_env()
+
+    def _init_jinja_env(self):
+        env = Environment(loader=FileSystemLoader(self.TEMPLATE_FOLDER), trim_blocks=True, lstrip_blocks=True)
+        env.filters['camel_to_snake'] = camel_to_snake
+        # TODO: find a proper way to convert HTML tags to reStructuredText format
+        env.filters['br_to_newline'] = lambda s: '| ' + s.replace('<br>', '\n    | ') if '<br>' in s else s
+        return env
+
+    def generate_model_index(self, include_models=None):
+        self._clean_generated_files(self.MODELS_FOLDER)
+
+        for model_name, operations in self._api_spec[SpecProp.MODEL_OPERATIONS].items():
+            ignore_model = include_models and model_name not in include_models
+            # TODO: investigate why some operations still have None model name
+            if model_name is None or ignore_model:
+                continue
+
+            model_api_spec = self._api_spec[SpecProp.MODELS].get(model_name, {})
+            model_spec = ModelSpec(
+                model_name,
+                model_api_spec.get(PropName.DESCRIPTION, ''),
+                model_api_spec.get(PropName.PROPERTIES, {}),
+                operations.keys()
+            )
+            model_content = self._jinja_env.get_template(self.MODEL_TEMPLATE).render(model=model_spec)
+            self._write_generated_file(self.MODELS_FOLDER, model_name, model_content)
+
+    def generate_operation_index(self, include_models=None):
+        self._clean_generated_files(self.OPERATIONS_FOLDER)
+
+        for op_name, op_api_spec in self._api_spec[SpecProp.OPERATIONS].items():
+            ignore_op = include_models and op_api_spec[OperationField.MODEL_NAME] not in include_models
+            if ignore_op:
+                continue
+
+            op_spec = OperationSpec(
+                op_name,
+                op_api_spec.get(OperationField.DESCRIPTION),
+                op_api_spec.get(OperationField.PARAMETERS, {}).get(OperationParams.PATH, {}),
+                op_api_spec.get(OperationField.PARAMETERS, {}).get(OperationParams.QUERY, {}),
+            )
+            operation_content = self._jinja_env.get_template(self.MODEL_TEMPLATE).render(operation=op_spec)
+            self._write_generated_file(self.OPERATIONS_FOLDER, op_name, operation_content)
+
+    @staticmethod
+    def _write_generated_file(dir_path, filename, content):
+        with open('%s/%s.rst' % (dir_path, camel_to_snake(filename)), "wb") as f:
+            f.write(content.encode('utf-8'))
+
+    def _clean_generated_files(self, dir_path):
+        for file_name in os.listdir(dir_path):
+            if file_name != self.INDEX_FILE:
+                os.remove(os.path.join(dir_path, file_name))
+
+
 def camel_to_snake(text):
     test_with_underscores = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', text)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', test_with_underscores).lower()
-
-
-def clean_generated_files(dir_path):
-    for file_name in os.listdir(dir_path):
-        if file_name != INDEX_FILE:
-            os.remove(os.path.join(dir_path, file_name))
-
-
-def generate_model_index(api_spec, model_template, include_models=None):
-    clean_generated_files(MODELS_FOLDER)
-
-    for model_name, operations in api_spec[SpecProp.MODEL_OPERATIONS].items():
-        ignore_model = include_models and model_name not in include_models
-        # TODO: investigate why some operations still have None model name
-        if model_name is None or ignore_model:
-            continue
-
-        model_api_spec = api_spec[SpecProp.MODELS].get(model_name, {})
-        model_spec = ModelSpec(
-            model_name,
-            model_api_spec.get(PropName.DESCRIPTION, ''),
-            model_api_spec.get(PropName.PROPERTIES, {}),
-            operations.keys()
-        )
-        with open('%s/%s.rst' % (MODELS_FOLDER, camel_to_snake(model_name)), "wb") as f:
-            f.write(model_template.render(
-                model=model_spec
-            ).encode('utf-8'))
-
-
-def generate_operation_index(api_spec, operation_template, include_models=None):
-    clean_generated_files(OPERATIONS_FOLDER)
-
-    for op_name, op_spec in api_spec[SpecProp.OPERATIONS].items():
-        ignore_op = include_models and op_spec[OperationField.MODEL_NAME] not in include_models
-        if ignore_op:
-            continue
-
-        operation = OperationSpec(
-            op_name,
-            op_spec.get(OperationField.DESCRIPTION),
-            op_spec.get(OperationField.PARAMETERS, {}).get(OperationParams.PATH, {}),
-            op_spec.get(OperationField.PARAMETERS, {}).get(OperationParams.QUERY, {}),
-        )
-        with open('%s/%s.rst' % (OPERATIONS_FOLDER, camel_to_snake(op_name)), "wb") as f:
-            f.write(operation_template.render(
-                operation=operation
-            ).encode('utf-8'))
 
 
 def fetch_api_specs_with_docs(hostname, username, password):
@@ -103,15 +117,6 @@ def fetch_api_specs_with_docs(hostname, username, password):
     return FdmSwaggerParser().parse_spec(json.loads(to_text(spec_resp)), json.loads(to_text(docs_resp)))
 
 
-def init_jinja_env():
-    env = Environment(loader=FileSystemLoader(TEMPLATE_FOLDER), trim_blocks=True, lstrip_blocks=True,
-                      extensions=['jinja2.ext.do'])
-    env.filters['camel_to_snake'] = camel_to_snake
-    # TODO: find a proper way to convert HTML tags to reStructuredText format
-    env.filters['br_to_newline'] = lambda s: '| ' + s.replace('<br>', '\n    | ') if '<br>' in s else s
-    return env
-
-
 def main():
     parser = argparse.ArgumentParser(description='Generates Ansible modules from Swagger documentation')
     parser.add_argument('hostname', type=str, help='Hostname where FTD can be accessed')
@@ -120,11 +125,10 @@ def main():
     parser.add_argument('--models', type=str, nargs='+', help='A list of models to include in the docs', required=False)
     args = parser.parse_args()
 
-    env = init_jinja_env()
     api_spec = fetch_api_specs_with_docs(args.hostname, args.username, args.password)
-
-    generate_model_index(api_spec, env.get_template(MODEL_TEMPLATE), args.models)
-    generate_operation_index(api_spec, env.get_template(OPERATION_TEMPLATE), args.models)
+    doc_generator = DocGenerator(api_spec)
+    doc_generator.generate_model_index(args.models)
+    doc_generator.generate_operation_index(args.models)
 
 
 if __name__ == '__main__':
