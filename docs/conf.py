6570ab449b59c123883fce36cee58afc83fbf929
Anton Nikulin <anton.nikulin@teamdev.com>
2018-09-24 19:23:16 +0300

    Update Sphinx to support Markdown format

--- docs/conf.py
+++ docs/conf.py
@@ -47,8 +47,7 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
@@ -154,3 +153,7 @@ texinfo_documents = [
      author, 'ansible-ftd', 'One line description of project.',
      'Miscellaneous'),
 ]
+
+source_parsers = {
+    '.md': 'recommonmark.parser.CommonMarkParser',
+}
