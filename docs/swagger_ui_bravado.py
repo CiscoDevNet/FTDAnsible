3ded004bf98c5d5ff774dd5444bec6d02b32724f
119vik <vitalij.kostenko@gmail.com>
2018-12-05 19:16:41 +0200

    update bravado snippets generation

--- docs/swagger_ui_bravado.py
+++ docs/swagger_ui_bravado.py
@@ -3,22 +3,19 @@ from . import body_generation
 
 def generate_sample(op_name, op_spec, data_params, full_spec, jinja_env):
     template_name = "snippet_bravado.j2"
-    opertaion_arguments = [
-        '{}="{}"'.format(k, v['type'])
+    opertaion_arguments = {
+        k: '"{}"'.format(v['type'])
         for k,v in op_spec.get("parameters", {}).get('path', {}).items()
-    ]
+    }
 
     if data_params:
         body = body_generation.body_generator(data_params, full_spec)
-        opertaion_arguments.append(
-            'body={}'.format(
-                str(body).replace(', ', ", \n\t\t")
-            )
-        )
+        opertaion_arguments["body"] = str(body).replace(', ', ", \n\t\t")
+
     template = jinja_env.get_template(template_name)
     return template.render(
         var=op_spec['tags'][0].lower(),
         tag=op_spec['tags'][0],
         operation=op_name,
-        opertaion_arguments=", \n\t".join(opertaion_arguments)
+        opertaion_arguments=opertaion_arguments
     )
